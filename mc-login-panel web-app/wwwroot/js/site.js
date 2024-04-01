f6ec04c6af971b6181ecb9fc714b53ed08912925
DemonsSauls <saul.salesa.2001@gmail.com>
2024-04-01 10:31:52 +0200

    Agregar archivos de proyecto.

---
+++ mc-login-panel web-app/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿const galletaImage = document.getElementById('galleta');
+const beeImage = document.getElementById('bee');
+
+beeImage.addEventListener('click', () => {
+  galletaImage.style.display = 'block';
+});
+
+galletaImage.addEventListener('click', () => {
+  galletaImage.style.display = 'none';
+});
+
